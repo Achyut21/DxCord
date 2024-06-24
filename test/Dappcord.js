13be926b5e7169707cac0f48b877ade258d4b712
Achyut21 <achyutkatiyar21@gmail.com>
2024-06-24 21:27:06 +0530

    Working Code

--- test/Dappcord.js
+++ test/Dappcord.js
@@ -1,24 +1,33 @@
-const { ethers } = require("hardhat")
-const { expect } = require("chai")
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
+  return ethers.utils.parseUnits(n.toString(), 'ether');
 }
 
 describe("Dappcord", function () {
-  describe("Deployment", function(){
-    it("Sets the name", async() => {
-      const Dappcord = await ethers.getContractFactory("Dappcord")
-      Dappcord = await Dappcord.deploy("Dappcord","DC")
-      let result = await Dappcord.name()
-      expect(result).to.equal("Dappcord")
-    })
+  
+  let Dappcord;
 
-    it("Sets the symbol", async() => {
-      const Dappcord = await ethers.getContractFactory("Dappcord")
-      Dappcord = await Dappcord.deploy("Dappcord","DC")
-      let result = await Dappcord.symbol()
-      expect(result).to.equal("DC")
-    })
-  })
-})
+  
+  const NAME = "Dappcord";
+  const SYMBOL = "DC";
+  
+  beforeEach(async () => {
+    const DappcordFactory = await ethers.getContractFactory("Dappcord");
+    Dappcord = await DappcordFactory.deploy(NAME, SYMBOL);
+    await Dappcord.deployed();
+  });
+
+  describe("Deployment", function () {
+    it("Sets the name", async () => {
+      let result = await Dappcord.name();
+      expect(result).to.equal(NAME);
+    });
+
+    it("Sets the symbol", async () => {
+      let result = await Dappcord.symbol();
+      expect(result).to.equal(SYMBOL);
+    });
+  });
+});
